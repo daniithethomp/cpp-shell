277f74e2bb6f9c8f99394d375e3b683ebea53fb5
daniithompson <danii.thompson8@gmail.com>
2025-05-23 23:57:28 +0100

    fix minor formatting issue

--- src/cd_command.cpp
+++ src/cd_command.cpp
@@ -4,7 +4,7 @@
 
 void CdCommand::execute(const std::string& args) {
     if (chdir(args.c_str()) != 0) {
-        std::cerr << "cd " << args << ": No such file or directory" << std::endl;
+        std::cerr << "cd: " << args << ": No such file or directory" << std::endl;
     }
 }
 
