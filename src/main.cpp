ee3cd18d7b4b4a9d45b07436717d10722126d5e4
daniithompson <danii.thompson8@gmail.com>
2025-05-23 19:40:34 +0100

    simple repl

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,13 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
   std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+  while(true) {
+    std::cout << "$ ";
+    std::getline(std::cin, input);
+    if (input == "") {
+      break;
+    }
+    std::cout << input << ": command not found" << std::endl;
+  }
 }
