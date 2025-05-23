4d2c5ddac2b359715edacf6f2754e5f1cee5a845
daniithompson <danii.thompson8@gmail.com>
2025-05-23 21:08:07 +0100

    refactoring to use oop

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,7 @@
-#include <iostream>
-#include <string>
-
-void do_exit(std::string args) {
-    int exit_code = std::stoi(args);
-    exit(exit_code);
-}
-
-void do_echo(std::string args) {
-    std::cout << args << std::endl;
-}
+#include "shell.h"
 
 int main() {
-    // Flush after every std::cout / std:cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
-
-    std::string input;
-    do {
-        std::cout << "$ ";
-        std::getline(std::cin, input);
-        std::string cmd = input.substr(0, input.find(' '));
-        std::string args = input.substr(input.find(' ') + 1);
-        if (cmd == "exit") {
-            do_exit(args);
-        }
-        else if (cmd == "echo") {
-            do_echo(args);
-        }
-        else if (input != "") {
-            std::cout << input << ": command not found" << std::endl;
-        }
-    } while(input != "");
+    Shell shell;
+    shell.run();
+    return 0;
 }
