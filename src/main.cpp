5dd62f3de2e382f79d49650ac2c52eb8ec884fd0
daniithompson <danii.thompson8@gmail.com>
2025-05-23 19:57:00 +0100

    echo command

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <string>
 
+void do_exit(std::string args) {
+    int exit_code = std::stoi(args);
+    exit(exit_code);
+}
+
+void do_echo(std::string args) {
+    std::cout << args << std::endl;
+}
+
 int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
@@ -13,9 +22,12 @@ int main() {
         std::string cmd = input.substr(0, input.find(' '));
         std::string args = input.substr(input.find(' ') + 1);
         if (cmd == "exit") {
-            exit(std::stoi(args));
+            do_exit(args);
         }
-        if (input != "") {
+        else if (cmd == "echo") {
+            do_echo(args);
+        }
+        else if (input != "") {
             std::cout << input << ": command not found" << std::endl;
         }
     } while(input != "");
